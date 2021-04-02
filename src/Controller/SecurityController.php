ce9e0712e8946acdfdbc241a0dab7b9dad0cf0f8
novaedra <novaedra@gmail.com>
2021-04-02 16:37:27 +0200

    login & auth

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,15 +16,6 @@ use Symfony\Component\Serializer\Serializer;
 
 class SecurityController extends AbstractController
 {
-    /**
-     * @Route("/security", name="security")
-     */
-    public function index(): Response
-    {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
-        ]);
-    }
 
     public function addUser(Request $request): Response {
         $user = new User();
@@ -35,8 +27,30 @@ class SecurityController extends AbstractController
         if ($datas['first_name']){
             $user->setName($datas['first_name']);
         }
+    }
 
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+         if ($this->getUser()) {
+             return $this->redirectToRoute('home');
+         }
 
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the users
+        $lastUsername = $authenticationUtils->getLastUsername();
 
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
